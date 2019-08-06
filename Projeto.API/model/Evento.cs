96b124e5ac9da4aeaa5de0f9804e9f6a93901862
Jéssica Cruz <jessica.santos648@gmail.com>
2019-08-05 22:35:31 -0300

    API do projeto de estudo

---
+++ Projeto.API/model/Evento.cs
@@ -0,0 +1,15 @@
+namespace Projeto.API.model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+
+        public string ImagemURL { get; set; }
+        public int QtdPessoas { get; set; }
+        public string Lote { get; set; }
+
+    }
+}
